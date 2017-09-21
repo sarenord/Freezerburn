5d6905507af96030d077cfca5b1edfa744c31319
sarenord <ajsheld12@gmail.com>
2017-09-21 14:57:49 -0400

    fixed capitalization error

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+import (
+	"fmt"
+//	"strings"
+	"io/ioutil"
+	"encoding/json"
+	"os"
+	"os/signal"
+	"time"
+	"github.com/bwmarrin/discordgo"
+)
+
+type Object struct {
+	Token string
+	Guild string
+}
+
+
+
+var bot Object
+var err error
+var start int64
+
+
+
+
+
+
+
+func main() {
+	start=time.Now().UnixNano()
+	//load config
+	io, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	json.Unmarshal(io, &bot)
+
+	//generate the session
+	discordSession, err := discordgo.New("Bot " + bot.Token)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//handlers
+	discordSession.AddHandler(ready)
+	discordSession.AddHandler(messageCreate)
+
+	//make the socket
+	err = discordSession.Open()
+	if err !=  nil{
+		fmt.Println(err)
+		return
+	}
+
+
+	//somehow this kills the program on CTRL+C
+	fmt.Println("bot is now running.")
+	//this creates a channel to send the signal
+	//to the actual program
+	sc := make(chan os.Signal, 1)
+	//if the signal is os.Interrupt, pass it to the program? maybe?
+	signal.Notify(sc, os.Interrupt)
+	<-sc
+	discordSession.Close()
+}
+
+func ready(s *discordgo.Session, event *discordgo.Ready) {
+	fmt.Println("received ready signal")
+	return
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	//ignore the bot's own messages
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	fmt.Println(m.Content)}
+
+
---
+++ structs.go
@@ -0,0 +1,4 @@
+type Object struct {
+	Token string
+	Guild string
+}
